00e5276278f8f37f71216321720fb57f5e169964
MCJack123 <jackmacwindowslinux@gmail.com>
2022-09-21 18:06:34 -0400

    Added license; can now be used as CLI tool

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include <QApplication>
 #include <QStyleFactory>
 
+extern int sanjuuni_main(int argc, const char * argv[]);
+
 #undef main
 int main(int argc, char *argv[]) {
+    if (argc > 1) return sanjuuni_main(argc, (const char **)argv);
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
